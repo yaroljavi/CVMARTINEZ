612efe861dfb7b23ac136d121853ee26c5e0e079
dam <dam@203P16>
2023-02-17 09:39:04 +0100

    ALFA

---
+++ src/examen/Clase1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+    
+    private final String nombre = "Yarol Javier";
+    private final String apellidos = "Martínez González";
+    
+}
