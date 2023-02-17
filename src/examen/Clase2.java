cc467c8750827040f575adf2dfe9215896381392
dam <dam@203P16>
2023-02-17 09:47:12 +0100

    BETA

---
+++ src/examen/Clase2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    
+    private final String rama = "Estoy en la rama 'PRODUCCIÓN'";
+    private final String texto = "Esta es la segunda línea";
+    
+}
